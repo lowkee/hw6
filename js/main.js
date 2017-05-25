91131ad49772f88b08e4f01f9a8004f1cf99d1c1
Collin Kee <collinkee@gmail.com>
2017-05-24 18:55:17 -0700

    new file

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var people []
+
+$(document).ready(function(){
+  $("#newPerson").click(function(){
+  $("td").append("<li>Add New Person</li>");
+  });
+});
+
+function myFunction() {
+   var x =
+  document.getElementById("fname").form.id;
+  document.getElementById("lname").form.id
+  document.getElementById("age").innerHTML = x;
+}
+
+function form() {
+	var fname = $('#fname').val();
+	var lname = $('#lname').val();
+    var age = $('#age').val();
+	if(description && quantity) {
+		items.push({
+			'fname': fname,
+			'lname': lname,
+            'age'  : age
+		});
+		
+	}
+	event.preventDefault();
+}
+var people = { people: fname, lname, age };
+localStorage.setItem("people", JSON.stringify(people));
+var retrievedData = localStorage.getItem("people");
+
